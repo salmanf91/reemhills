97d515ae9d61963495c47abdb0d7ff5e455b2764
aboobakarsinan <aboobakarsinan@users.noreply.github.com>
2024-02-27 11:02:47 +0530

    Feature/feb changes 2 (#10)

--- app/Http/Controllers/SalesForceController.php
+++ app/Http/Controllers/SalesForceController.php
@@ -6,27 +6,44 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
 class SalesforceController extends Controller
-{
+{    
+    public static $token_url;
+    public static $client_id;
+    public static $client_secret;
+    public static $username;
+    public static $password;
+    public static $api_url;
     
+    public static function initialize()
+    {
+        self::$token_url = config('sfconfig.sandbox.token_url');
+        self::$client_id = config('sfconfig.sandbox.client_id');
+        self::$client_secret = config('sfconfig.sandbox.client_secret');
+        self::$username = config('sfconfig.sandbox.username');
+        self::$password = config('sfconfig.sandbox.password');
+        self::$api_url = config('sfconfig.sandbox.api_url');
+
+    }
+
     public static function getToken()
     {
+        self::initialize();
         $client = new Client();
-
+           
         try {
-            $response = $client->post(env('SALESFORCE_TOKEN_URL'), [
+            $response = $client->post(self::$token_url, [
                 'form_params' => [
                     'grant_type' => 'password',
-                    'client_id' => env('SALESFORCE_CLIENT_ID'),
-                    'client_secret' => env('SALESFORCE_CLIENT_SECRET'),
-                    'username' => env('SALESFORCE_USERNAME'),
-                    'password' => env('SALESFORCE_PASSWORD')
+                    'client_id' => self::$client_id,
+                    'client_secret' => self::$client_secret,
+                    'username' => self::$username,
+                    'password' => self::$password
                 ],
                 'verify' => false
             ]);
             
             $data = json_decode($response->getBody()->getContents(), true);
             $token = $data['access_token'];
-            
             return $token;
             
         } catch (\Exception $e) {
@@ -35,18 +52,19 @@ class SalesforceController extends Controller
     }
 
     public static function postData($data)
-    {
+    {        
+        dd($data);
         $token = self::getToken();
-
         if ($token) {
             $client = new Client();
             
             try {
 
-                $response = $client->post(env('SALESFORCE_API_URL'), [
+                $response = $client->post(self::$api_url, [
                     'headers' => [
                         'Authorization' => 'Bearer ' . $token,
-                        'Content-Type' => 'application/json'
+                        'Content-Type' => 'application/json',
+                        'Accept' => 'application/json'
                     ],
                     'json' => $data,
                     'verify' => false
@@ -54,7 +72,6 @@ class SalesforceController extends Controller
                 
                 $data = json_decode($response->getBody()->getContents(), true);
                 // Process response data as needed
-                
                 return response()->json($data);
                 
             } catch (\Exception $e) {
